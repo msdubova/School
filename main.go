de0408cc5e6588ba3965b373622353415d334011
Your GitHub Usernamemsdubova <femme777@gmail.com>
2024-06-16 11:17:07 +0100

    Added listen 8080

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/note", getNote)
+
+	http.ListenAndServe(":8080", mux)
 }
 
 func getNote(w http.ResponseWriter, r *http.Request) {}
