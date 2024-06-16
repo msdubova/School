24ab2c644e1591fd7400af4101814b3375194b8a
Your GitHub Usernamemsdubova <femme777@gmail.com>
2024-06-16 11:15:37 +0100

    Mux

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main () {
-	
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/note", getNote)
 }
+
+func getNote(w http.ResponseWriter, r *http.Request) {}
