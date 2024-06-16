be2293a8844faa6e31eefbdb6552745b46b45ac3
Your GitHub Usernamemsdubova <femme777@gmail.com>
2024-06-16 12:15:24 +0100

    Hahdlers: get put

--- main.go
+++ main.go
@@ -1,22 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type Student struct {
+	Name      string
+	Id        int
+	UpdatedAt time.Time
+}
+
+type Class []Student
+
+var John = Student{
+	Name:      "John",
+	Id:        2323,
+	UpdatedAt: time.Now(),
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/note", getNote)
+
+	mux.HandleFunc("/class", processNote)
 
 	err := http.ListenAndServe(":8080", mux)
-
 	if err != nil {
 		fmt.Println("Error happened", err.Error())
 		return
 	}
+
+}
+
+func processNote(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getNote(w, r)
+	case http.MethodPut:
+		putNote(w, r)
+	}
 }
 
 func getNote(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Welcome back to school on 8080")
+	err := json.NewEncoder(w).Encode(John)
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func putNote(w http.ResponseWriter, r *http.Request) {
+
 }
