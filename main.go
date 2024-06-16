91e2b1dc5e5d9008ba0102a2728a1a1b5bfd01a4
Your GitHub Usernamemsdubova <femme777@gmail.com>
2024-06-16 12:54:55 +0100

    Added authentification

--- main.go
+++ main.go
@@ -16,15 +16,26 @@ type Student struct {
 type Class []Student
 
 var John = Student{
-	Name:      "John",
-	Id:        2323,
-	UpdatedAt: time.Now(),
+	Name: "John",
+	Id:   2323,
 }
+
+var Emma = Student{
+	Name: "Emma",
+	Id:   3232,
+}
+
+var David = Student{
+	Name: "David",
+	Id:   6676,
+}
+
+var toddlers = Class{John, Emma, David}
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/class", processNote)
+	mux.HandleFunc("/class", checkAuth(processNote))
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
@@ -38,13 +49,13 @@ func processNote(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		getNote(w, r)
-	case http.MethodPut:
-		putNote(w, r)
+		// case http.MethodPut:
+		// 	putNote(w, r)
 	}
 }
 
 func getNote(w http.ResponseWriter, r *http.Request) {
-	err := json.NewEncoder(w).Encode(John)
+	err := json.NewEncoder(w).Encode(toddlers)
 	if err != nil {
 		fmt.Println(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
@@ -52,6 +63,44 @@ func getNote(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func putNote(w http.ResponseWriter, r *http.Request) {
+//	func putNote(w http.ResponseWriter, r *http.Request) {
+//		err := json.NewDecoder(r.Body).Decode(&John)
+//		if err != nil {
+//			fmt.Println(err.Error())
+//			w.WriteHeader(http.StatusBadRequest)
+//			return
+//		}
+//		John.UpdatedAt = time.Now()
+//		err = json.NewEncoder(w).Encode(John)
+//		if err != nil {
+//			fmt.Println(err.Error())
+//			w.WriteHeader(http.StatusInternalServerError)
+//			return
+//		}
+//	}
+type User struct {
+	Name string
+	Role string
+}
 
+var teacher = User{
+	Name: "MrBond",
+	Role: "teacher",
+}
+
+func checkAuth(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name, role, ok := r.BasicAuth()
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		if teacher.Name != name || teacher.Role != role {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
 }
